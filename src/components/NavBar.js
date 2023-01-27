10d50199ccd8ba9bba2462ed176c337d6bb44580
Veridiana Maricato <maricato.veridiana@gmail.com>
2023-01-27 18:48:52 -0300

    navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,33 @@
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
-import logo from '../img/logo_branco.png'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+
+    const [isMobile, setMobile] = useState(false)
     return (
-        <header className={styles.menu_principal}>
+        <nav className={styles.navbar}>
             <div className={styles.logo}></div>
+            <ul className={isMobile ? styles.nav_links_mobile : styles.nav_links}
+                onClick={() => setMobile(false)}
+                >
+                <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : styles.nav_link)}>Home</NavLink>
+                <NavLink to='/contact' className={({ isActive }) => (isActive ? styles.active : styles.nav_link)}>Contato</NavLink>
+                <NavLink to='/recepies' className={({ isActive }) => (isActive ? styles.active : styles.nav_link)}>Receitas</NavLink>
+                <NavLink to='/order' className={({ isActive }) => (isActive ? styles.active : styles.nav_link)}>Pedir</NavLink>
+                <NavLink to='/login' className={({ isActive }) => (isActive ? styles.active : styles.nav_link)}>Login</NavLink>
+                <NavLink to='/register' className={({ isActive }) => (isActive ? styles.active : styles.nav_link)}>Cadastrar-se</NavLink>
+            </ul>
             <div>
-                <nav className={styles.menu_principal__lista}>
-                    <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : "")}>Home</NavLink>
-                    <NavLink to='/contact' className={({ isActive }) => (isActive ? styles.active : "")}>Contato</NavLink>
-                    <NavLink to='/recepies' className={({ isActive }) => (isActive ? styles.active : "")}>Receitas</NavLink>
-                    <NavLink to='/order' className={({ isActive }) => (isActive ? styles.active : "")}>Pedir</NavLink>    
-                    <NavLink to='/login' className={({ isActive }) => (isActive ? styles.active : "")}>Login</NavLink>    
-                    <NavLink to='/register' className={({ isActive }) => (isActive ? styles.active : "")}>Cadastrar-se</NavLink>    
-                </nav>
+                <button className={styles.mobile_menu_icon}
+                onClick={() => setMobile(!isMobile)}
+                >
+                    {isMobile ? <i class="fa fa-times"></i> : <i class="fa fa-bars"></i>}
+
+                </button>
             </div>
-        </header>
+        </nav>
     )
 }
 
